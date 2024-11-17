e088f0758abca00b138e15f046b6c21a8d989009
Lukas2019 <freead@t-online.de>
2024-11-17 15:32:59 +0100

    websocked chat is working now

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,11 @@
+from calendar import c
 from django.contrib import admin
+from pyparsing import C
 from .models import Chat, Message
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('id','user1', 'user2')
+
 # Register your models here.
-admin.site.register(Chat)
+admin.site.register(Chat, ChatAdmin)
 admin.site.register(Message)
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,9 @@
-
 import json
+from math import e
 from channels.generic.websocket import AsyncWebsocketConsumer
-
-import user
-
-
+from django.core.exceptions import ValidationError
 from .models import Chat, Message
-import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.db import database_sync_to_async
 
 class UnreadMessagesCountConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -84,38 +80,63 @@ class UnreadMessagesGeneralCountConsumer(AsyncWebsocketConsumer):
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
-        if self.scope["user"].is_anonymous:
+        self.user = self.scope["user"]
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # Überprüfen, ob der Benutzer authentifiziert ist
+        if self.user.is_anonymous:
             await self.close()
         else:
-            self.room_group_name = f"{self.scope['url_route']['kwargs']['room_name']}"
-            print(f"\"{self.room_group_name}\"")
-            # Join room group
-            await self.channel_layer.group_add(
-                self.room_group_name,
-                self.channel_name
-            )
+            try:
+                # Überprüfen, ob die Chat-ID gültig ist und der Benutzer Teil des Chats ist
+                chat = await self.get_chat(self.room_name)
+                if not await self.is_user_in_chat(chat, self.user):
+                    raise ValidationError("User is not part of the chat.")
+                
+                # Join room group
+                await self.channel_layer.group_add(
+                    self.room_group_name,
+                    self.channel_name
+                )
 
-            await self.accept()
+                await self.accept()
+            except (Chat.DoesNotExist, ValidationError) as e:
+                print(f"Connection rejected: {e}")
+                await self.close()
 
     async def disconnect(self, close_code):
-        pass
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     async def receive(self, text_data):
-        pass
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        self.send(text_data=json.dumps({"message": message}))
-    
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_message',
+                'user': self.user.id.hex,
+                'message': message,
+            }
+        )
+
     async def send_message(self, event):
         message = event["message"]
-        user_id = event["user_id"]
-        print("send_message", message)
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            "message": message, 
-            "user_id": user_id
-            }))
+        await self.send(text_data=json.dumps(event))
+
+    @database_sync_to_async
+    def get_chat(self, room_name):
+        return Chat.objects.get(id=room_name)
+
+    @database_sync_to_async
+    def is_user_in_chat(self, chat, user):
+        return user in [chat.user1, chat.user2]
